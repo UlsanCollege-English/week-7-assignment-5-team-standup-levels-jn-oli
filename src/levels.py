4549e660881405cf56de4d83f4dab9c0372b5f49
jn-oli <zaynwoli18@gmail.com>
2025-10-29 13:51:24 +0900

    changed

--- src/levels.py
+++ src/levels.py
@@ -1,3 +1,5 @@
+# src/levels.py
+
 from collections import deque
 
 class Node:
@@ -6,14 +8,63 @@ class Node:
         self.left = left
         self.right = right
 
+
 def level_order(root):
-    # TODO: return list of lists
-    pass
+    """Return the level-order traversal of a binary tree."""
+    if not root:
+        return []
+    res = []
+    queue = deque([root])
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        res.append(level)
+    return res
+
 
 def zigzag_level_order(root):
-    # TODO
-    pass
+    """Return the zigzag level-order traversal of a binary tree."""
+    if not root:
+        return []
+    res = []
+    queue = deque([root])
+    left_to_right = True
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        if not left_to_right:
+            level.reverse()
+        res.append(level)
+        left_to_right = not left_to_right
+    return res
+
 
 def right_side_view(root):
-    # TODO
-    pass
+    """Return the list of nodes visible from the right side."""
+    if not root:
+        return []
+    view = []
+    queue = deque([root])
+    while queue:
+        n = len(queue)
+        for i in range(n):
+            node = queue.popleft()
+            if i == n - 1:
+                view.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return view
